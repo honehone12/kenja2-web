66b718d994afbfa1fd1c89f01f6ecd8e3a6509c0
honehone <honehone.41games@gmail.com>
2025-06-11 17:38:38 +0900

    added interfaces

---
+++ src/lib/server/api-adapter/api-adapters.ts
@@ -0,0 +1,25 @@
+import { type Encoder, type Decoder } from "$lib/server/endec/endec"
+
+export interface TextSearchPayload {
+
+}
+
+export interface VectorSearchPayload {
+    
+}
+
+export interface TextSearchResult {
+
+}
+
+export interface VectorSearchResult {
+
+}
+
+export interface ApiAdapter<
+    E extends Encoder, 
+    D extends Decoder
+> {
+    TextSearch(payload: TextSearchPayload): Promise<TextSearchResult>
+    VectorSearch(payload: VectorSearchPayload): Promise<VectorSearchResult>
+}
---
+++ src/lib/server/endec/endec.ts
@@ -0,0 +1,7 @@
+export interface Encoder {
+
+}
+
+export interface Decoder {
+    
+}
