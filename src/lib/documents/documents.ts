faca47908547eda51dc02a0441e107b847edf4bd
honehone <honehone.41games@gmail.com>
2025-06-11 21:40:43 +0900

    added documents

--- src/lib/documents/documents.ts
+++ src/lib/documents/documents.ts
@@ -1,2 +1,17 @@
 'use strict';
 
+export interface Parent {
+    id: string;
+    name: string;
+    name_japanese: string | undefined;
+}
+
+export interface Candidate{
+    id: string;
+    url: string;
+    parent: Parent | undefined;
+    name: string;
+    name_english: string | undefined;
+    name_japanese: string | undefined;
+    aliases: string[] | undefined;
+}
--- src/lib/documents/queries.ts
+++ src/lib/documents/queries.ts
@@ -1,37 +1,75 @@
 'use strict';
 
+import type { Candidate } from "$lib/documents/documents";
+
 export const enum Rating {
-    Unspecified = 0,
     AllAges = 1,
     Hentai = 2
 }
 
+export class RatingConv {
+    static fromString(s: string) {
+        switch (s) {
+        case 'all-ages':
+            return Rating.AllAges;
+        case 'hentai':
+            return Rating.Hentai;
+        default:
+            return undefined;
+        }
+    }
+}
+
 export const enum VectorField {
-    Unspecified = 0,
     Txt = 1,
     Img = 2
 }
 
+export class VFieldConv {
+    static fromString(s: string) {
+        switch (s) {
+        case 'text-vector':
+            return VectorField.Txt;
+        case 'image-vector':
+            return VectorField.Img
+        default:
+            undefined
+        }
+    }
+}
+
 export const enum ItemType {
-    Unspecified = 0,
     Anime = 1,
     Character = 2
 }
 
+export class ITypeConv {
+    static fromString(s: string) {
+        switch (s) {
+        case 'anime':
+            return ItemType.Anime;
+        case 'character':
+            return ItemType.Character;
+        default:
+            undefined
+        }
+    }
+}
+
 export interface TextQuery {
-    rating: Rating;
-    item_type: ItemType;
-    keywords: string
+    rating: Rating | undefined;
+    item_type: ItemType | undefined;
+    keywords: string;
 }
 
 export interface VectorQuery {
-    rating: Rating,
-    item_type: ItemType,
-    source_field: VectorField,
-    target_field: VectorField,
-    id: string
+    rating: Rating | undefined;
+    item_type: ItemType | undefined;
+    source_field: VectorField | undefined;
+    target_field: VectorField | undefined;
+    id: string;
 }
 
 export interface QueryResult {
-
+    candidates: Candidate[] | undefined;
 }
