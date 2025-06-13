f8c387043b288dd6f830760c6f498c262f416058
honehone <honehone.41games@gmail.com>
2025-06-13 13:51:39 +0900

    added root error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    'use strict';
+
+    import ErrorMsg from "$lib/components/ErrorMsg.svelte";
+</script>
+
+<div class="hero w-full h-screen">
+    <div class="hero-content">
+        <ErrorMsg />
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,3 @@
     'use strict'
 
 </script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
