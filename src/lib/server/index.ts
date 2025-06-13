123c42153c458dfc0a8800eac64b9ce1b8e38646
honehone <honehone.41games@gmail.com>
2025-06-13 12:19:57 +0900

    fixed #2

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -8,6 +8,7 @@ import type { Decoder, Encoder } from "$lib/server/endec/endec";
 import  { Json } from "$lib/server/endec/json";
 import { HttpApi } from "$lib/server/api-adapter/http-api";
 import { LambdaApi, type LambdaFunctions } from "$lib/server/api-adapter/lambda-api";
+import logs from "./logs";
 
 class Kenja {
     private _api: ApiAdapter | null = null;
@@ -17,7 +18,7 @@ class Kenja {
         D extends Decoder
     >(e: E, d: D) {
         if (this._api) {
-            throw new Error('api is already initialized');
+            throw new Error('api client is already initialized');
         }
 
         const baseUrl = env.HTTP_API_BASE_URL;
@@ -33,7 +34,7 @@ class Kenja {
         D extends Decoder
     >(e: E, d: D) {
         if (this._api) {
-            throw new Error('api is already initialized');
+            throw new Error('api client is already initialized');
         }
 
         const textSearch = env.LAMBDA_TEXT_SEARCH;
@@ -60,7 +61,7 @@ class Kenja {
 
     api(): ApiAdapter {
         if (!this._api) {
-            throw new Error('api adapter is not initialized');
+            throw new Error('api client is not initialized');
         }
         return this._api;
     }
@@ -69,11 +70,14 @@ class Kenja {
 const kenja = new Kenja();
 
 if (!building) {
-    // just throw and fail on initialization error
-    kenja.useHttp(
-        new Json(), 
-        new Json()
-    );
+    try {
+        kenja.useHttp(
+            new Json(), 
+            new Json()
+        );    
+    } catch (err) {
+        logs.error(err, 'api client initialization failed');
+    }
 }
 
 export default kenja;
